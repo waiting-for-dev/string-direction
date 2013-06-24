dddc89b0b9bdfab2a39dff993d526f58d27d1cea
marc <marc@lamarciana.com>
2013-06-24 13:34:01 +0200

    clean and comment code

--- lib/string-direction/string.rb
+++ lib/string-direction/string.rb
@@ -8,19 +8,25 @@ class String
   # @return ["right"] if it's a right-to-left string
   # @return ["bidi"] if it's a bi-directinal string
   def direction
-    if self.has_ltr_mark? and self.has_rtl_mark?
+    if has_ltr_mark? and has_rtl_mark?
       'bidi'
-    elsif self.has_ltr_mark?
+    elsif has_ltr_mark?
       'left'
-    elsif self.has_rtl_mark?
+    elsif has_rtl_mark?
       'right'
     end
   end
 
+  # returns whether self contains the unicode left-to-right mark
+  #
+  # @return [Boolean] true if it containts ltr mark, false otherwise
   def has_ltr_mark?
     self.match(/^(.*)#{LTR_MARK}(.*)$/) ? true : false
   end
 
+  # returns whether self contains the unicode right-to-left mark
+  #
+  # @return [Boolean] true if it containts rtl mark, false otherwise
   def has_rtl_mark?
     self.match(/^(.*)#{RTL_MARK}(.*)$/) ? true : false
   end
