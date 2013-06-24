c31ba348249e3727a1a52f657e902dd09a170d40
marc <marc@lamarciana.com>
2013-06-24 13:10:05 +0200

    direction returns "right" when there is only rtl marl

--- lib/string-direction/string.rb
+++ lib/string-direction/string.rb
@@ -1,8 +1,15 @@
 class String
+  LTR_MARK = "\u200e"
+  RTL_MARK = "\u200f"
+
   # returns the direction in which self is written
   #
   # @return ["left"] if it's a left-to-right string
   def direction
-    'left' if self.match /^(.*)\u200e(.*)$/
+    if self.match /^(.*)#{LTR_MARK}(.*)$/
+      'left'
+    elsif self.match /^(.*)#{RTL_MARK}(.*)$/
+      'right'
+    end
   end
 end
--- spec/string/string_spec.rb
+++ spec/string/string_spec.rb
@@ -1,3 +1,4 @@
+#encoding: UTF-8
 require 'spec_helper'
 
 describe String do
@@ -5,6 +6,11 @@ describe String do
     it "should return 'left' if it contains the left-to-right mark and no right-to-left mark" do
       string = "\u200eHola"
       string.direction.should eql 'left'
+    end
+
+    it "should return 'right' if it contains the right-to-left mark and no left-to-right mark" do
+      string = "\u200fالعربية"
+      string.direction.should eql 'right'
     end
   end
 end
