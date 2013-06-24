e1dba06ecc73daea1c54112aab5499855ced546f
marc <marc@lamarciana.com>
2013-06-24 13:29:44 +0200

    refactor test

--- spec/string/string_spec.rb
+++ spec/string/string_spec.rb
@@ -1,20 +1,25 @@
 #encoding: UTF-8
 require 'spec_helper'
 
+ENGLISH = 'English'
+ARABIC = 'العربية'
+LTR_MARK = "\u200e"
+RTL_MARK = "\u200f"
+
 describe String do
   describe "#direction" do
     it "should return 'left' if it contains the left-to-right mark and no right-to-left mark" do
-      string = "\u200eEnglish"
+      string = LTR_MARK+ENGLISH
       string.direction.should eql 'left'
     end
 
     it "should return 'right' if it contains the right-to-left mark and no left-to-right mark" do
-      string = "\u200fالعربية"
+      string = RTL_MARK+ARABIC
       string.direction.should eql 'right'
     end
 
     it "should return 'bidi' if it contains the left-to-right mark and the right-to-left mark" do
-      string = "\u200eEnglish"+"\u200fالعربية"
+      string = LTR_MARK+ENGLISH+RTL_MARK+ARABIC
       string.direction.should eql 'bidi'
     end
   end
