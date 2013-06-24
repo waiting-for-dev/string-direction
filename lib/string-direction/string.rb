e83efd766961060e94c3c350d891aa8ad9b4e5f3
marc <marc@lamarciana.com>
2013-06-24 15:23:37 +0200

    direction returns left if no mark and no rtl character is present

--- lib/string-direction/string.rb
+++ lib/string-direction/string.rb
@@ -1,6 +1,35 @@
 class String
+  # left-to-right unicode mark
   LTR_MARK = "\u200e"
+
+  # right-to-left unicode mark
   RTL_MARK = "\u200f"
+
+  # right-to-left characters unicode blocks
+  RTL_UNICODE_BLOCKS = [
+    # Hebrew main letters
+    "\u0590-\u05ff",
+    # Arabic main letters
+    "\u0600-\u06ff",
+    # Syriac main letters
+    "\u0700-\u074f",
+    # Arabic supplement
+    "\u0750-\u077f",
+    # Thaana main letters
+    "\u0780-\u07bf",
+    # N'Ko main letters
+    "\u07c0-\u07ff",
+    # Samaritan main letters
+    "\u0800-\u083f",
+    # Arabic extended
+    "\u08a0-\u08ff",
+    # Hebrew presentation form
+    "\ufb1d-\ufb4f",
+    # Arabic presentation form 1
+    "\ufb50-\ufdff",
+    # Arabic presentation form 2
+    "\ufe70-\ufeff",
+  ]
 
   # returns the direction in which self is written
   #
@@ -14,6 +43,8 @@ class String
       'left'
     elsif has_rtl_mark?
       'right'
+    elsif not has_rtl_characters?
+      'left'
     end
   end
 
@@ -21,13 +52,20 @@ class String
   #
   # @return [Boolean] true if it containts ltr mark, false otherwise
   def has_ltr_mark?
-    self.match(/^(.*)#{LTR_MARK}(.*)$/) ? true : false
+    match(/^(.*)#{LTR_MARK}(.*)$/) ? true : false
   end
 
   # returns whether self contains the unicode right-to-left mark
   #
   # @return [Boolean] true if it containts rtl mark, false otherwise
   def has_rtl_mark?
-    self.match(/^(.*)#{RTL_MARK}(.*)$/) ? true : false
+    match(/^(.*)#{RTL_MARK}(.*)$/) ? true : false
+  end
+
+  # returns whether self contains some right-to-left character
+  #
+  # @return [Boolean] true if it containts rtl characters, false otherwise
+  def has_rtl_characters?
+    match(Regexp.union(*RTL_UNICODE_BLOCKS)) ? true : false
   end
 end
--- spec/string/string_spec.rb
+++ spec/string/string_spec.rb
@@ -8,19 +8,27 @@ RTL_MARK = "\u200f"
 
 describe String do
   describe "#direction" do
-    it "should return 'left' if it contains the left-to-right mark and no right-to-left mark" do
-      string = LTR_MARK+ENGLISH
-      string.direction.should eql 'left'
-    end
+    context "when marks are present" do
+      it "should return 'left' if it contains the left-to-right mark and no right-to-left mark" do
+        string = LTR_MARK+ENGLISH
+        string.direction.should eql 'left'
+      end
 
-    it "should return 'right' if it contains the right-to-left mark and no left-to-right mark" do
-      string = RTL_MARK+ARABIC
-      string.direction.should eql 'right'
-    end
+      it "should return 'right' if it contains the right-to-left mark and no left-to-right mark" do
+        string = RTL_MARK+ARABIC
+        string.direction.should eql 'right'
+      end
 
-    it "should return 'bidi' if it contains the left-to-right mark and the right-to-left mark" do
-      string = LTR_MARK+ENGLISH+RTL_MARK+ARABIC
-      string.direction.should eql 'bidi'
+      it "should return 'bidi' if it contains both the left-to-right mark and the right-to-left mark" do
+        string = LTR_MARK+ENGLISH+RTL_MARK+ARABIC
+        string.direction.should eql 'bidi'
+      end
+    end
+    context "when marks are not present" do
+      it "should return 'left' if no right-to-left character is present" do
+        string = ENGLISH
+        string.direction.should eql 'left'
+      end
     end
   end
 end
