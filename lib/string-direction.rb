99ff56d0c3cd113bd9f5d739a0e4b475a98540a3
marc <marc@lamarciana.com>
2015-09-03 17:22:58 +0200

    Avoid monkey patching and use methods with string argument

--- lib/string-direction.rb
+++ lib/string-direction.rb
@@ -1,4 +1,109 @@
-require 'string-direction/version'
-require 'string-direction/string-direction'
+# encoding: UTF-8
 
-String.send :include, StringDirection
+require 'string-direction/version'
+
+# Module with all the logic for automatic detection of text direction
+module StringDirection
+  LTR_MARK = "\u200e".freeze # left-to-right unicode mark
+  RTL_MARK = "\u200f".freeze # right-to-left unicode mark
+
+  # Regular expressions used to match direction markers
+  LTR_MARK_REGEX    = /#{LTR_MARK}/.freeze # String contains a LTR marker
+  RTL_MARK_REGEX    = /#{RTL_MARK}/.freeze # String contains a RTL marker
+  CHAR_IGNORE_REGEX = /[\p{M}\p{P}\p{S}\p{Z}\p{C}]/.freeze # Ignore unicode marks, punctuations, symbols, separator and other general categories
+
+  # Direction strings that get passed around
+  BIDI = 'bidi'.freeze
+  RTL  = 'rtl'.freeze
+  LTR  = 'ltr'.freeze
+
+  class << self
+    attr_accessor :rtl_scripts
+
+    # rtl_scripts
+    def rtl_scripts
+      @rtl_scripts ||= %w(Arabic Hebrew Nko Kharoshthi Phoenician Syriac Thaana Tifinagh)
+    end
+
+    # given an array of script names, which should be supported by Ruby {http://www.ruby-doc.org/core-1.9.3/Regexp.html#label-Character+Properties regular expression properties}, returns a string where all of them are concatenaded inside a "\\p{}" construction
+    #
+    # @param [Array] scripts the array of script names
+    # @return [String] the script names joined ready to be used in the construction of a regular expression
+    # @example
+    #   StringDirection.join_scripts_for_regexp(%w(Arabic Hebrew)) #=> "\p{Arabic}\p{Hebrew}"
+    def join_scripts_for_regexp(scripts)
+      scripts.map { |script| '\p{'+script+'}' }.join
+    end
+
+    # returns the direction in which a string is written
+    #
+    # @return ["ltr"] if it's a left-to-right string
+    # @return ["rtl"] if it's a right-to-left string
+    # @return ["bidi"] if it's a bi-directinal string
+    def direction(string)
+      if has_ltr_mark?(string) && has_rtl_mark?(string)
+        BIDI
+      elsif has_ltr_mark?(string)
+        LTR
+      elsif has_rtl_mark?(string)
+        RTL
+      elsif !has_rtl_characters?(string)
+        LTR
+      elsif has_ltr_characters?(string)
+        BIDI
+      else
+        RTL
+      end
+    end
+
+    # whether string is a left-to-right one
+    #
+    # @return [Boolean] true if it is a left-to-right string, false otherwise
+    def is_ltr?(string)
+      direction(string) == LTR
+    end
+
+    # whether string is a right-to-left one
+    #
+    # @return [Boolean] true if it is a right-to-left string, false otherwise
+    def is_rtl?(string)
+      direction(string) == RTL
+    end
+
+    # whether string is a bi-directional one
+    #
+    # @return [Boolean] true if it is a bi-directional string, false otherwise
+    def is_bidi?(string)
+      direction(string) == BIDI
+    end
+
+    # returns whether string contains the unicode left-to-right mark
+    #
+    # @return [Boolean] true if it containts ltr mark, false otherwise
+    def has_ltr_mark?(string)
+      string.match(LTR_MARK_REGEX) ? true : false
+    end
+
+    # returns whether string contains the unicode right-to-left mark
+    #
+    # @return [Boolean] true if it containts rtl mark, false otherwise
+    def has_rtl_mark?(string)
+      string.match(RTL_MARK_REGEX) ? true : false
+    end
+
+    # returns whether string contains some right-to-left character
+    #
+    # @return [Boolean] true if it containts rtl characters, false otherwise
+    def has_rtl_characters?(string)
+      string.match(/[#{StringDirection::join_scripts_for_regexp(StringDirection.rtl_scripts)}]/) ? true : false
+    end
+
+    # returns whether string contains some left-to-right character
+    #
+    # @return [Boolean] true if it containts ltr characters, false otherwise
+    def has_ltr_characters?(string)
+      # ignore unicode marks, punctuations, symbols, separator and other general categories
+      string.gsub(CHAR_IGNORE_REGEX, '').match(/[^#{StringDirection::join_scripts_for_regexp(StringDirection.rtl_scripts)}]/) ? true : false
+    end
+  end
+end
--- spec/string_direction_spec.rb
+++ spec/string_direction_spec.rb
@@ -1,7 +1,133 @@
+# encoding: UTF-8
+
 require 'spec_helper'
 
 describe StringDirection do
   it 'has a version number' do
     expect(StringDirection::VERSION).not_to be nil
+  end
+
+  let(:english) { 'English' }
+  let(:arabic) { 'العربية' }
+
+  describe '.direction(string)' do
+    context 'when marks are present' do
+      it "returns 'ltr' if it contains the left-to-right mark and no right-to-left mark" do
+        string = described_class::LTR_MARK + english
+
+        expect(described_class.direction(string)).to eql 'ltr'
+      end
+
+      it "returns 'rtl' if it contains the right-to-left mark and no left-to-right mark" do
+        string = described_class::RTL_MARK + arabic
+
+        expect(described_class.direction(string)).to eql 'rtl'
+      end
+
+      it "returns 'bidi' if it contains both the left-to-right mark and the right-to-left mark" do
+        string = described_class::LTR_MARK + english + described_class::RTL_MARK + arabic
+
+        expect(described_class.direction(string)).to eql 'bidi'
+      end
+    end
+
+    context 'when marks are not present' do
+      it "returns 'ltr' if no right-to-left character is present" do
+        string = english
+
+        expect(described_class.direction(string)).to eql 'ltr'
+      end
+
+      it "returns 'rtl' if only right-to-left character are present" do
+        string = arabic
+
+        expect(described_class.direction(string)).to eql 'rtl'
+      end
+
+      it "returns 'bidi' if both left-to-right and right-to-left characters are present" do
+        string = arabic + ' ' + english
+
+        expect(described_class.direction(string)).to eql 'bidi'
+      end
+    end
+
+    context 'when default rtl scripts are changed' do
+      let(:new_rtl_script) { 'Latin' }
+      let(:old_rtl_script) { 'Arabic' }
+
+      it "returns 'rtl' if there are characters for an added right-to-left script and no marks characters are present" do
+        StringDirection.rtl_scripts << new_rtl_script
+        string = english
+
+        expect(described_class.direction(string)).to eql 'rtl'
+      end
+
+      it "returns 'ltr' if there are characters for a deleted right-to-left script (so now ltr) and no mark characters are present" do
+        StringDirection.rtl_scripts.delete old_rtl_script
+        string = arabic
+
+        expect(described_class.direction(string)).to eql 'ltr'
+      end
+
+      after :each do
+        StringDirection.rtl_scripts.delete new_rtl_script if StringDirection.rtl_scripts.include? new_rtl_script
+        StringDirection.rtl_scripts << old_rtl_script unless StringDirection.rtl_scripts.include? old_rtl_script
+      end
+
+      context 'when special characters are present' do
+        it 'ignores special characters for the direction detection' do
+          mark = "\u0903"
+          punctuation = '_'
+          symbol = '€'
+          separator = ' '
+          other = "\u0005"
+          string = arabic + mark + punctuation + symbol + separator + other
+
+          expect(described_class.direction(string)).to eql 'rtl'
+        end
+      end
+    end
+  end
+
+  describe '#is_ltr?' do
+    it 'returns true if it is a left-to-right string' do
+      string = english
+
+      expect(described_class.is_ltr?(string)).to eq(true)
+    end
+
+    it 'returns false if it is not a left-to-right string' do
+      string = arabic
+
+      expect(described_class.is_ltr?(string)).to eq(false)
+    end
+  end
+
+  describe '#is_rtl?' do
+    it 'returns true if it is a right-to-left string' do
+      string = arabic
+
+      expect(described_class.is_rtl?(string)).to eq(true)
+    end
+
+    it 'returns false if it is not a right-to-left string' do
+      string = english
+
+      expect(described_class.is_rtl?(string)).to eq(false)
+    end
+  end
+
+  describe '#is_bidi?' do
+    it 'returns true if it is a bi-directional string' do
+      string = english + ' ' + arabic
+
+      expect(described_class.is_bidi?(string)).to eq(true)
+    end
+
+    it 'returns false if it is not a bi-directional string' do
+      string = english
+
+      expect(described_class.is_bidi?(string)).to eq(false)
+    end
   end
 end
