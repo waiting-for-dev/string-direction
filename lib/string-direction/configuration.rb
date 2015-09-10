a1a77631583507066ce4593e039dbc2f533af3a6
marc <marc@lamarciana.com>
2015-09-10 13:27:50 +0200

    Add config option default_analyzers

--- lib/string-direction/configuration.rb
+++ lib/string-direction/configuration.rb
@@ -1,9 +1,10 @@
 module StringDirection
   class Configuration
-    attr_accessor :rtl_scripts
+    attr_accessor :rtl_scripts, :default_analyzers
 
     def initialize
       self.rtl_scripts = %w(Arabic Hebrew Nko Kharoshthi Phoenician Syriac Thaana Tifinagh)
+      self.default_analyzers = [:marks, :characters]
     end
 
     def reset
--- spec/string-direction/configuration_spec.rb
+++ spec/string-direction/configuration_spec.rb
@@ -2,16 +2,14 @@ require 'spec_helper'
 
 describe StringDirection::Configuration do
   describe '#rtl_scripts' do
-    it 'returns by default an array with Arabic, Hebrew, Nko, Kharoshthi, Phoenician, Syriac, Thaana and Tifinagh' do
+    it 'defaults to an array with Arabic, Hebrew, Nko, Kharoshthi, Phoenician, Syriac, Thaana and Tifinagh' do
       expect(subject.rtl_scripts).to match_array(%w(Arabic Hebrew Nko Kharoshthi Phoenician Syriac Thaana Tifinagh))
     end
   end
 
-  describe '#rtl_scripts=' do
-    it 'assigns to rtl_scripts instance variable' do
-      subject.rtl_scripts = %w(Arabic)
-
-      expect(subject.rtl_scripts).to eq(%w(Arabic))
+  describe '#default_analyzers' do
+    it 'defaults to an array with :marks and :characters' do
+      expect(subject.default_analyzers).to eq([:marks, :characters])
     end
   end
 
