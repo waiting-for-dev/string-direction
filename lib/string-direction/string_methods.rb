4b91cedd494cb4fc903a6187264e8b621539e0d4
marc <marc@lamarciana.com>
2015-09-11 12:22:13 +0200

    Document and tweak StringMethods

--- lib/string-direction/string_methods.rb
+++ lib/string-direction/string_methods.rb
@@ -1,25 +1,31 @@
 module StringDirection
+  # Methods intended to be monkey patched to String through `String.include(StringDirection::StringMethods)`. This will allow stuff like `'English'.direction #=> 'ltr'`. All methods are delegated to {Detector} with `self` as string argument.
   module StringMethods
+    # @see Detector#direction
+    # @return [String]
     def direction
-      detector.direction(self)
+      string_direction_detector.direction(self)
     end
 
+    # @see Detector#ltr?
     def ltr?
-      detector.ltr?(self)
+      string_direction_detector.ltr?(self)
     end
 
+    # @see Detector#rtl?
     def rtl?
-      detector.rtl?(self)
+      string_direction_detector.rtl?(self)
     end
 
+    # @see Detector#bidi?
     def bidi?
-      detector.bidi?(self)
+      string_direction_detector.bidi?(self)
     end
 
     private
 
-    def detector
-      @detector ||= StringDirection::Detector.new
+    def string_direction_detector
+      @string_direction_detector ||= StringDirection::Detector.new
     end
   end
 end
