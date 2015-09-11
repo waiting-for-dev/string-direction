c0ea7754a2be9e2acc2d5899f5c39d6df4d11473
marc <marc@lamarciana.com>
2015-09-11 11:11:35 +0200

    Document Detector class

--- lib/string-direction/detector.rb
+++ lib/string-direction/detector.rb
@@ -1,7 +1,14 @@
 module StringDirection
+  # String direction detector
   class Detector
+    # Array of strategies used, in order, to try to detect the direction of a string
+    #
+    # @return [Array]
     attr_accessor :strategies
 
+    # Initialize strategies from given arguments. If no strategies are given, they are taken from the value of {StringDirection::Configuration#default_strategies}
+    #
+    # @raise [ArgumentError] if strategy class is not found. For example, for an strategy `:marks` a class `StringDirection::MarksStrategy` is expected
     def initialize(*strategies)
       if strategies.empty?
         self.strategies = StringDirection.configuration.default_strategies
@@ -11,6 +18,10 @@ module StringDirection
       check_strategies
     end
 
+    # Tries to detect and return the direction of a string. It returns `ltr` if the string is left-to-right, `rtl` if it is right-to-left, `bidi` if it is bidirectional or `nil` if it can't detect the direction. It iterates through {#strategies} until one of them successes.
+    #
+    # @param string [String] The string to inspect
+    # @return [String]
     def direction(string)
       direction = nil
       strategies.each do |strategy|
@@ -20,14 +31,26 @@ module StringDirection
       direction
     end
 
+    # Returns whether string is left-to-right or not
+    #
+    # @param string [String] The string to inspect
+    # @return [Boolean]
     def ltr?(string)
       direction(string) == StringDirection::LTR
     end
 
+    # Returns whether string is right-to-left or not
+    #
+    # @param string [String] The string to inspect
+    # @return [Boolean]
     def rtl?(string)
       direction(string) == StringDirection::RTL
     end
 
+    # Returns whether string is bidirectional or not
+    #
+    # @param string [String] The string to inspect
+    # @return [Boolean]
     def bidi?(string)
       direction(string) == StringDirection::BIDI
     end
